ed8fbaac2ab4117471e7d894284718ca8736d433
Javier <javier.arancibia.reyes@hotmail.com>
2020-11-10 11:52:22 -0300

    ADDED category and type to views with relation with estate model

--- app/models/estate.rb
+++ app/models/estate.rb
@@ -2,6 +2,8 @@ class Estate < ApplicationRecord
     belongs_to :user
     has_many_attached :photos
     belongs_to :location, optional: true
+    belongs_to :category, optional: true
+    belongs_to :type, optional: true
 
     scope :latest, -> { order created_at: :desc }
 end
