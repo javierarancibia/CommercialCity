eefe6df5c8f4cd2ac55e5182b7a0bf992565ef8d
Javier <javier.arancibia.reyes@hotmail.com>
2020-11-11 00:15:30 -0300

    finished local omniauth with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable, 
+         :omniauthable, :omniauth_providers => [:facebook]
          
   has_one_attached :avatar
   has_many :estates
@@ -14,4 +15,13 @@ class User < ApplicationRecord
       "default_profileB.jpg"
     end
   end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    user.first_name = auth.info.name
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   get 'dashboard/reports'
 
   resources :estates
-  devise_for :users
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions'
+  }
   root to: 'public#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
