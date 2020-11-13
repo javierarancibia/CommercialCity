533ef9c1421d4af0731a292bbfec88e64094d215
Javier <javier.arancibia.reyes@hotmail.com>
2020-11-13 13:58:21 -0300

    FIXED seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2819,37 +2819,27 @@ Estate.create(
     [
         {
             user_id: 1,
-        },
-        {
-            address: "Libertad 55"
-        },
-        {
+       
+            address: "Libertad 55",
+       
             price: 25000000,
-        },
-        {
+        
             rooms: 3,
-        },
-        {
+        
             bathrooms: 2,
-        },
-        {
+        
             parking: 2,
-        },
-        {
+       
             storage: 1,
-        },
-        {
+        
             description: "Propiedad muy bonita",
-        },
-        {
+       
             location_id: 10,
-        },
-        {
-            category_id: 2
-        },
-        {
-            type_id: 1,
-        },
+
+            category_id: 2,
+       
+            type_id: 1
+        }
     ]
 )
 
