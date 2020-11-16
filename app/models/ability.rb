4154437b89f282e4442dd80c21a2e85a8bf72aca
Javier <javier.arancibia.reyes@hotmail.com>
2020-11-16 17:23:15 -0300

    Fixed to deploy

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,12 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-      user ||= User.new # guest user (not logged in)
-      if user.role("admin")?
-        can :manage, :all
-      else
-        can :read, :all
-      end
+      # user ||= User.new # guest user (not logged in)
+      # if user.role("admin")?
+      #   can :manage, :all
+      # else
+      #   can :read, :all
+      # end
 
       # can [:destroy, :update], Estate, user_id: user.id
     #
